f3378c968353f07f5267e2695b98c24056b30a5b
Y0rFa1se <neargul123@gmail.com>
2025-08-08 03:30:55 +0900

    test

--- modules/decorators.py
+++ modules/decorators.py
@@ -1,10 +1,15 @@
 from telegram import Update
 from telegram.ext import ContextTypes
 
+import os
+
 def command(cmd):
     def decorator(func):
         async def wrapper(update: Update, context: ContextTypes.DEFAULT_TYPE):
-            result = await func(update)
+            result = None
+            if str(update.effective_chat.id).strip() == os.getenv("CHAT_ID"):
+                result = await func(update)
+
             return result
         
         wrapper.command = cmd
@@ -16,14 +21,19 @@ def callback(callback_data):
     def decorator(func):
         async def wrapper(update: Update, context: ContextTypes.DEFAULT_TYPE):
             query = update.callback_query
-            await query.answer()
 
-            if query.data == callback_data:
-                result = await func(update)
-                return result
-            
-            else:
-                await query.edit_message_text("‼️잘못된 요청입니다‼️")
+            result = None
+            if str(query.message.chat.id).strip() == os.getenv("CHAT_ID"):
+                await query.answer()
+
+                if query.data == callback_data:
+                    result = await func(update)
+                    return result
+                
+                else:
+                    await query.edit_message_text("‼️잘못된 요청입니다‼️")
+
+            return result
 
         wrapper.decorator = "callback"
         return wrapper
